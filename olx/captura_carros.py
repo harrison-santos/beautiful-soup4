3499b88f8bcc5977591336b28f7ab26622936fec
harrison-santos <harrisonspeed1997@gmail.com>
2018-12-18 11:05:19 -0300

    Captura dos carros e escrita em csv.

--- olx/captura_carros.py
+++ olx/captura_carros.py
@@ -1,48 +1,61 @@
+from unicodedata import normalize
 import requests
+import os.path
+import csv
 from bs4 import BeautifulSoup
 
-termos_essenciais = ['Modelo:', 'Ano:', 'Cor:', 'Portas:', 'Potência do motor:', 'Combustível:',
-                     'Quilometragem:', 'Direção:', 'Câmbio:']# 9 termos
-termos_descartados = ['Categoria:', 'Final de placa:', 'Único dono:', 'Aceita Trocas:', 'Tipo de veículo:']
+def trata_termo(termo):
+    return remover_acentos(termo.replace(':', '').lower().replace('do ', '').replace('de ', '').replace(' ', '_'))
 
+def trata_descricao(descricao):
+    return descricao.replace(' portas', '')
+
+def remover_acentos(txt):
+    return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
+
+
+#CONF
+
+termos_essenciais = ['id_post', 'modelo', 'ano', 'cor', 'portas', 'potencia_motor', 'combustivel',
+                     'quilometragem', 'direcao', 'cambio', 'preco']# 9 termos
+termos_descartados = ['Categoria', 'Final de placa', 'Único dono', 'Aceita Trocas', 'Tipo de veículo']
 parametros = {'q': 'carros'}
 file = requests.get("https://se.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios", params=parametros)
 #print(file.url)
 soup = BeautifulSoup(file.content, 'html.parser')
-#soup = soup.find_all("ul", {"id": "main-ad-list"})
-#print()
 cont = 0
+##
 
-##CAPTURA DOS LINKS
+qtd_carros = int(input('Quantidade de carros: '))
+
+#CAPTURA DOS LINKS
 lista_links = []
 for ul in soup.find_all("ul", {"id": "main-ad-list"}):
     for li in ul.find_all('li'):
         if(li.has_attr('data-list_id')):
             lista_links.append(li.a['href'])
             cont+= 1
-        if cont == 1:
+        if cont == qtd_carros:
             break;
-#print(cont)
-#print(lista_links)
 ##
 
 
-lista_atributos = []
-lista_carros = []
-lista_termos = []
-lista_opcionais = []
+#PERCORRENDO LINKS
 for link in lista_links:
+    lista_atributos = {}
+    lista_carros = []
+    lista_termos = []
+    lista_opcionais = {}
     item = requests.get(link)
     soup = BeautifulSoup(item.content, 'html.parser')
-    id_carro = soup.find('div', {'class': 'OLXad-id'}).p.strong.text
+    id_post = soup.find('div', {'class': 'OLXad-id'}).p.strong.text
     preco = soup.find('span', {'class': 'actual-price'})
-    #modelo = soup.find('span', string='Modelo:').next_sibling.next_sibling.a.text.strip()
-    print("Modelooo")
-    #print(modelo)
-    ano = ''
+    lista_atributos['id_post'] = id_post
+
     if(preco is not None):
-        print("ID: "+id_carro)
-        print("Preço: "+preco.contents[0])
+        preco = preco.text.replace('R$ ', '')
+        #print("ID: "+id_post)
+        #print("Preço: "+preco)
 
         ul_detalhes = soup.find('div', {'class': 'OLXad-details'}).div.ul.find_all('li')
         #print(ul_detalhes
@@ -50,21 +63,32 @@ for link in lista_links:
             if (li_detalhes.parent['class'][0] == 'OLXad-features-list'):
                 termo = 'Opcional: '
                 descricao = li_detalhes.text.strip()
-                lista_opcionais.append(descricao)
+                lista_opcionais[descricao] = 'SIM'
             else:
-                termo = li_detalhes.span.text.strip()
-                descricao = li_detalhes.span.next_sibling.next_sibling.text.strip()
+                termo = trata_termo(li_detalhes.span.text.strip())
+                descricao = trata_descricao(li_detalhes.span.next_sibling.next_sibling.text.strip())
 
 
             if(termo in termos_essenciais):
+                if(termo == 'quilometragem'):
+                    descricao = descricao.split('.')[0]
                 lista_termos.append(termo)
-                lista_atributos.append((termo, descricao))
-                print(termo + descricao)
+                lista_atributos[termo] = descricao
+                #print(termo + descricao)
 
 
-
+        lista_atributos['preco'] = preco
         print(lista_termos)
         print(lista_atributos)
         print(lista_opcionais)
-        if(len(lista_termos) == len(termos_essenciais)):
-           pass
+        print("Essenciais: {}, Capturados: {}".format(len(termos_essenciais), len(lista_atributos)))
+
+        if(len(lista_atributos) == len(termos_essenciais)):
+           arquivo_existe = os.path.isfile('base_carros.csv')
+           with open('base_carros.csv', mode='a') as csv_file:
+               fieldnames = ['id_post', 'modelo', 'ano', 'cor', 'portas', 'potencia_motor', 'combustivel', 'quilometragem', 'direcao', 'cambio', 'preco']
+               writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+               if not arquivo_existe:
+                   writer.writeheader()
+               writer.writerow(lista_atributos)
+##
