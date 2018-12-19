00a27b5f8774d676e2eb0c612986505366de48e3
harrison-santos <harrisonspeed1997@gmail.com>
2018-12-19 09:55:35 -0300

    Captura de veículos definindo quantidade de páginas.

--- olx/captura_carros.py
+++ olx/captura_carros.py
@@ -13,89 +13,70 @@ def trata_descricao(descricao):
 def remover_acentos(txt):
     return normalize('NFKD', txt).encode('ASCII', 'ignore').decode('ASCII')
 
-
 #CONF
 termos_opcionais = ['air_bag', 'alarme', 'ar_condicionado', 'trava_eletrica', 'vidro_eletrico', 'som', 'sensor_re', 'camera_re']
-termos_essenciais = ['id_post', 'modelo', 'ano', 'cor', 'portas', 'potencia_motor', 'combustivel',
-                     'quilometragem', 'direcao', 'cambio', 'preco']# 9 termos
+termos_essenciais = ['id_post', 'modelo', 'ano', 'cor', 'portas', 'potencia_motor', 'combustivel','quilometragem', 'direcao', 'cambio', 'preco']# 9 termos
 termos_descartados = ['Categoria', 'Final de placa', 'Único dono', 'Aceita Trocas', 'Tipo de veículo']
-parametros = {'q': 'carros'}
-file = requests.get("https://se.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios", params=parametros)
-#print(file.url)
-soup = BeautifulSoup(file.content, 'html.parser')
-cont = 0
-##
 
-qtd_carros = int(input('Quantidade de carros: '))
+num_paginas = int(input('Numero de páginas: '))
+for i in range(0, num_paginas):
+    print('>> Página {} <<'.format(i+1))
+    parametros = {'q': 'carros', 'o': str(i+1)}
+    file = requests.get("https://se.olx.com.br/autos-e-pecas/carros-vans-e-utilitarios", params=parametros)
+    #print(file.url)
+    soup = BeautifulSoup(file.content, 'html.parser')
+    cont = 0
 
-#CAPTURA DOS LINKS
-lista_links = []
-for ul in soup.find_all("ul", {"id": "main-ad-list"}):
-    for li in ul.find_all('li'):
-        if(li.has_attr('data-list_id')):
-            lista_links.append(li.a['href'])
-            cont+= 1
-        if cont == qtd_carros:
-            break;
-##
+    #CAPTURA DOS LINKS
+    lista_links = []
+    for ul in soup.find_all("ul", {"id": "main-ad-list"}):
+        for li in ul.find_all('li'):
+            if(li.has_attr('data-list_id')):
+                lista_links.append(li.a['href'])
 
+    #PERCORRENDO LINKS
+    for link in lista_links:
+        lista_atributos = {}
+        lista_carros = []
+        lista_termos = []
+        lista_opcionais = []
+        item = requests.get(link)
+        soup = BeautifulSoup(item.content, 'html.parser')
+        id_post = soup.find('div', {'class': 'OLXad-id'}).p.strong.text
+        preco = soup.find('span', {'class': 'actual-price'})
+        lista_atributos['id_post'] = id_post
 
-#PERCORRENDO LINKS
-for link in lista_links:
-    lista_atributos = {}
-    lista_carros = []
-    lista_termos = []
-    lista_opcionais = []
-    item = requests.get(link)
-    soup = BeautifulSoup(item.content, 'html.parser')
-    id_post = soup.find('div', {'class': 'OLXad-id'}).p.strong.text
-    preco = soup.find('span', {'class': 'actual-price'})
-    lista_atributos['id_post'] = id_post
+        if(preco is not None):
+            preco = preco.text.replace('R$ ', '')
+            ul_detalhes = soup.find('div', {'class': 'OLXad-details'}).div.ul.find_all('li')
+            for li_detalhes in ul_detalhes:
+                if (li_detalhes.parent['class'][0] == 'OLXad-features-list'):
+                    descricao = trata_termo(li_detalhes.text.strip())
+                    lista_opcionais.append(descricao)
+                else:
+                    termo = trata_termo(li_detalhes.span.text.strip())
+                    descricao = trata_descricao(li_detalhes.span.next_sibling.next_sibling.text.strip())
 
-    if(preco is not None):
-        preco = preco.text.replace('R$ ', '')
-        #print("ID: "+id_post)
-        #print("Preço: "+preco)
+                if(termo in termos_essenciais):
+                    lista_termos.append(termo)
+                    lista_atributos[termo] = descricao
 
-        ul_detalhes = soup.find('div', {'class': 'OLXad-details'}).div.ul.find_all('li')
-        #print(ul_detalhes
-        for li_detalhes in ul_detalhes:
-            if (li_detalhes.parent['class'][0] == 'OLXad-features-list'):
-                termo = 'Opcional: '
-                descricao = trata_termo(li_detalhes.text.strip())
-                lista_opcionais.append(descricao)
-            else:
-                termo = trata_termo(li_detalhes.span.text.strip())
-                descricao = trata_descricao(li_detalhes.span.next_sibling.next_sibling.text.strip())
+            for opcional in termos_opcionais:
+                if(opcional in lista_opcionais):
+                    lista_atributos[opcional] = 'SIM'
+                else:
+                    lista_atributos[opcional] = 'NAO'
 
+            lista_atributos['preco'] = preco
+            num_opcionais = len(termos_opcionais)
+            print(lista_atributos)
+            print("Essenciais: {}, Capturados: {}".format(len(termos_essenciais), len(lista_atributos)-num_opcionais))
 
-            if(termo in termos_essenciais):
-                lista_termos.append(termo)
-                lista_atributos[termo] = descricao
-                #print(termo + descricao)
-
-
-        for opcional in termos_opcionais:
-            if(opcional in lista_opcionais):
-                lista_atributos[opcional] = 'SIM'
-            else:
-                lista_atributos[opcional] = 'NAO'
-
-        lista_atributos['preco'] = preco
-        num_opcionais = len(termos_opcionais) # +1 preco
-        print(lista_termos)
-        print(lista_atributos)
-        print(lista_opcionais)
-        print("Essenciais: {}, Capturados: {}".format(len(termos_essenciais), len(lista_atributos)-num_opcionais))
-
-
-        if(len(termos_essenciais) == (len(lista_atributos)-num_opcionais)):
-           arquivo_existe = os.path.isfile('base_carros.csv')
-           with open('base_carros.csv', mode='a') as csv_file:
-               fieldnames = ['id_post', 'modelo', 'ano', 'cor', 'portas', 'potencia_motor', 'combustivel', 'quilometragem', 'direcao', 'cambio',
-                             'air_bag', 'alarme', 'ar_condicionado', 'trava_eletrica', 'vidro_eletrico', 'som', 'sensor_re', 'camera_re', 'preco']
-               writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-               if not arquivo_existe:
-                   writer.writeheader()
-               writer.writerow(lista_atributos)
-##
+            if(len(termos_essenciais) == (len(lista_atributos)-num_opcionais)):
+               arquivo_existe = os.path.isfile('base_carros.csv')
+               with open('base_carros.csv', mode='a') as csv_file:
+                   fieldnames = ['id_post', 'modelo', 'ano', 'cor', 'portas', 'potencia_motor', 'combustivel', 'quilometragem', 'direcao', 'cambio', 'air_bag', 'alarme', 'ar_condicionado', 'trava_eletrica', 'vidro_eletrico', 'som', 'sensor_re', 'camera_re', 'preco']
+                   writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+                   if not arquivo_existe:
+                       writer.writeheader()
+                   writer.writerow(lista_atributos)
